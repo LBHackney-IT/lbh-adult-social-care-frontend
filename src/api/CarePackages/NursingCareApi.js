9ab6dc02e9bc90807a7166ba4dd9156ed2c94bdb
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-04-23 10:11:54 +0300

    Add api implementation to get and create nursing care package in  API

--- src/api/CarePackages/NursingCareApi.js
+++ src/api/CarePackages/NursingCareApi.js
@@ -16,7 +16,52 @@ const getNursingCareTypeOfStayOptions = () => {
     .catch(handleError);
 }
 
+const createNursingCarePackage = (nursingCarePackageForCreation) => {
+  const options = {
+    url: `${NURSING_CARE_URL}`,
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    data: nursingCarePackageForCreation
+  };
+  return axios(options)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+/*const updateNursingCarePackage = (nursingCarePackageId, nursingCarePackageForUpdate) => {
+  const options = {
+    url: `${NURSING_CARE_URL}/${nursingCarePackageId}`,
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    data: nursingCarePackageForUpdate
+  };
+  return axios(options)
+    .then(handleResponse)
+    .catch(handleError);
+}*/
+
+const getNursingCarePackageList = () => {
+  return axios.get(`${NURSING_CARE_URL}/get-all`)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+const getSingleNursingCarePackage = (nursingCarePackageId) => {
+  return axios.get(`${NURSING_CARE_URL}/${nursingCarePackageId}`)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
 export {
   getTypeOfNursingHomeOptions,
-  getNursingCareTypeOfStayOptions
+  getNursingCareTypeOfStayOptions,
+  createNursingCarePackage,
+  getNursingCarePackageList,
+  getSingleNursingCarePackage
 }
