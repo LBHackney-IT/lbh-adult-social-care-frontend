bd1a2d53614fc9b766e204194509f621ab37a7ab
Peter Madeley <peterthomasen97@outlook.com>
2021-07-08 02:27:14 +0100

    Remove slice references

--- reducers/combinedReducers.js
+++ reducers/combinedReducers.js
@@ -3,16 +3,12 @@ import userReducer from "./userReducer";
 import brokerageReducer from "./brokerageReducer";
 import supplierDashboardReducer from "./supplierDashboardReducer";
 import supplierReturnsReducer from "./supplierReturnsReducer";
-import homeCareTimeSlotShiftsReducer from "../pages/care-package/home-care/slices/homeCareTimeSlotShiftsSlice";
-import homeCareServicesReducer from "../pages/care-package/home-care/slices/homeCareServicesSlice";
 
 const reducers = {
   user: userReducer,
   brokerage: brokerageReducer,
   supplierDashboard: supplierDashboardReducer,
   supplierReturns: supplierReturnsReducer,
-  homeCareTimeSlotShifts: homeCareTimeSlotShiftsReducer,
-  homeCareServices: homeCareServicesReducer,
 };
 
 export default combineReducers(reducers);
