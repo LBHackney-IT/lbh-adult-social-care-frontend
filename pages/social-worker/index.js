0611d308db43c4d07e821d03dbbc9e3adc435b5b
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-07-22 23:02:47 +0100

    add router

--- pages/social-worker/index.js
+++ pages/social-worker/index.js
@@ -5,9 +5,9 @@ import HackneyFooterInfo from '../../components/HackneyFooterInfo';
 import { getUserSession } from '../../service/helpers';
 import withSession from '../../lib/session';
 import SocialWorkerInputs from '../../components/SocialWorker/SocialWorkerInputs';
-import { socialWorkerDashboardTableData } from '../../testData/testDataSocialWorker';
 import SocialWorkerTable from '../../components/SocialWorker/SocialWorkerTable';
 import { getSubmittedPackages, getSubmittedPackagesStatus } from '../../api/ApproversHub/SocialWorkerApi';
+import { RESIDENTIAL_CARE_ROUTE, NURSING_CARE_ROUTE} from '../../routes/RouteConstants';
 
 export const getServerSideProps = withSession(async ({ req }) => {
   const user = getUserSession({ req });
@@ -22,8 +22,10 @@ export const getServerSideProps = withSession(async ({ req }) => {
 
 const SocialWorkerDashboardPage = () => {
   const [sorts] = useState([
-    { name: 'client', text: 'Client' },
-    { name: 'category', text: 'Category' },
+    { name: 'client', text: 'Client'},
+    { name: 'packageId', text: 'Package ID' },
+    { name: 'primarySupport', text: 'Primary Support Reason' },
+    { name: 'category', text: 'Category', value: 'categoryId' },
     { name: 'dob', text: 'DOB' },
     { name: 'approver', text: 'Approver' },
     { name: 'submitted', text: 'Submitted\n(days ago)' },
@@ -41,7 +43,12 @@ const SocialWorkerDashboardPage = () => {
   };
 
   const onClickTableRow = (rowItems) => {
-    router.push(`${router.pathname}/${rowItems.id}`);
+    console.log(rowItems);
+    {rowItems.categoryId === 3 ? (
+      router.push(`${RESIDENTIAL_CARE_ROUTE}/${rowItems.packageId}`)
+    ) : (
+      router.push(`${NURSING_CARE_ROUTE}/${rowItems.packageId}`)
+    )}
   };
 
   const [socialWorkerData, setSubmittedPackages] = useState([]);
@@ -64,6 +71,9 @@ const SocialWorkerDashboardPage = () => {
         const pagedData = res.pagingMetaData
         const options = res.data.map((option) => ({
           client: option.client,
+          packageId: option.packageId,
+          primarySupportReason : option.primarySupportReason,
+          categoryId : option.categoryId,
           category : option.category,
           dateOfBirth : option.dateOfBirth,
           approver : option.approver,
