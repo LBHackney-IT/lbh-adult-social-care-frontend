7d2da19ddd0822b99c75399fcfdb93e1e553d013
Peter Madeley <peterthomasen97@outlook.com>
2021-07-23 11:16:54 +0100

    Remove values

--- api/BaseApi.js
+++ api/BaseApi.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_AWS_ENDPOINT || 'https://zqf7j796y5.execute-api.eu-west-2.amazonaws.com/staging/api';
+  process.env.NEXT_PUBLIC_AWS_ENDPOINT;
 
 // Redundant API key (to be removed)
-const AWS_KEY = process.env.NEXT_PUBLIC_AWS_KEY || '28e2GuYzKp2XhLCF448iw95LN2pngp3uahe9C6BE';
+const AWS_KEY = process.env.NEXT_PUBLIC_AWS_KEY;
 const AUTH_HEADER = { 'x-api-key': AWS_KEY };
 
 axios.interceptors.request.use((config) => {
