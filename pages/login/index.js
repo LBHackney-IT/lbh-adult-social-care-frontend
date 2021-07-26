3c93f1a6aaed2e394a2ffa2a517255e8a0d7852b
Vadim Kirpun <quinze105@gmail.com>
2021-07-26 17:26:59 +0300

    fix origin on Login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,25 +1,22 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
-import absoluteUrl from 'next-absolute-url';
-import { Button } from '../../components/Button';
+import React, { useEffect, useState } from 'react';
 import HackneyFooterInfo from '../../components/HackneyFooterInfo';
 import Header from '../../components/Layout/Header';
 import useUser from '../../lib/useUser';
 
 const hackneyAuthLink = 'https://auth.hackney.gov.uk/auth?redirect_uri=';
 
-export async function getServerSideProps({ req }) {
-  const { origin } = absoluteUrl(req);
-  return { props: { origin } };
-}
-
-const Login = ({ origin }) => {
+const Login = () => {
   const { mutateUser } = useUser({
     redirectTo: '/care-package',
     redirectIfFound: true,
   });
 
+  const [origin, setOrigin] = useState('');
+
   useEffect(() => {
+    setOrigin(window.location.origin);
+
     const login = async () => {
       try {
         mutateUser(await axios('/api/login'));
@@ -39,7 +36,7 @@ const Login = ({ origin }) => {
           <h2>Sign In</h2>
           <p>Please sign in with your Hackney email account.</p>
           <p>Please contact your manager if you have issues signing in.</p>
-          <a className='button button-base' href={`${hackneyAuthLink}${origin}`} target='_self'>
+          <a className='button button-base' href={`${hackneyAuthLink}${origin}/login`} target='_self'>
             <strong>Sign in with Google</strong>
           </a>
         </div>
