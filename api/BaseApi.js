946ccff32b584ef7c988da2f6d5d4c0bd5cc12f3
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-07-28 15:22:34 +0100

    fix staging url

--- api/BaseApi.js
+++ api/BaseApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_AWS_ENDPOINT;
 
 // Redundant API key (to be removed)
 const AWS_KEY = process.env.NEXT_PUBLIC_AWS_KEY;
