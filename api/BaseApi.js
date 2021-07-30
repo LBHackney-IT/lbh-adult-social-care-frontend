9ff71023dcbaf36d256bceb56d7a1effc4048a8d
Peter Madeley <peterthomasen97@outlook.com>
2021-07-30 12:57:13 +0100

    Change to direct ref

--- api/BaseApi.js
+++ api/BaseApi.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const { NEXT_PUBLIC_STAGE } = process.env;
-
 let baseUrl = '';
 let awsKey = '';
 
-switch (NEXT_PUBLIC_STAGE) {
+debugger;
+switch (process.env.NEXT_PUBLIC_STAGE) {
   case 'staging': {
     baseUrl = 'https://zqf7j796y5.execute-api.eu-west-2.amazonaws.com/staging/api';
     awsKey = '28e2GuYzKp2XhLCF448iw95LN2pngp3uahe9C6BE';
@@ -25,24 +24,6 @@ switch (NEXT_PUBLIC_STAGE) {
 }
 
 const BASE_URL = baseUrl;
-
-// switch (window.location.hostname) {
-//   case 'social-care-care-packages-staging.hackney.gov.uk': {
-//     BASE_URL = 'https://zqf7j796y5.execute-api.eu-west-2.amazonaws.com/staging/api';
-//     break;
-//   }
-//   case 'social-care-care-packages.hackney.gov.uk': {
-//     BASE_URL = 'https://dz58oqus03.execute-api.eu-west-2.amazonaws.com/production';
-//     break;
-//   }
-//   case 'localhost': {
-//     break;
-//   }
-//   default: {
-//     BASE_URL = '';
-//     break;
-//   }
-// }
 
 // Redundant API key (to be removed)
 const AUTH_HEADER = { 'x-api-key': awsKey };
