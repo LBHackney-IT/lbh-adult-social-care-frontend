9d301406304cf654fbe18599e4cc7fa64947eb33
Vadim Kabaev <v.kabaev@dunice.net>
2021-08-24 18:18:33 +0300

    deploy test fix

--- components/HackneyDS/index.js
+++ components/HackneyDS/index.js
@@ -4,7 +4,5 @@ import Radio from './Radio';
 import Fieldset from './Fieldset';
 import Accordion from './Accordion';
 import Announcement from './Announcement';
-import BackLink from './BackLink';
-import BackToTop from './BackToTop';
 
-export { Input, Button, Radio, Fieldset, Accordion, Announcement, BackLink, BackToTop };
+export { Input, Button, Radio, Fieldset, Accordion, Announcement };
