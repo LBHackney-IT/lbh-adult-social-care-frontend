545c01201912804552631dc4dd96a5385a55b1c7
Vadim Kabaev <v.kabaev@dunice.net>
2021-08-26 17:40:01 +0300

    slack fixes

--- components/HackneyDS/Button/index.js
+++ components/HackneyDS/Button/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
-export default function Button({ children, secondary, disabled, 'add-item': addItem, handler = () => {} }) {
+export default function Button({ children, secondary, disabled, link, 'add-item': addItem, handler = () => {} }) {
   const secondaryClassList = secondary ? ' govuk-secondary lbh-button--secondary' : '';
   const disabledClassList = disabled ? ' lbh-button--disabled govuk-button--disabled' : '';
   const addItemClassList = addItem ? ' lbh-button--add' : '';
-
   const addItemIcon = (
     <svg width="12" height="12" viewBox="0 0 12 12">
       <path d="M6.94 0L5 0V12H6.94V0Z" />
       <path d="M12 5H0V7H12V5Z" />
     </svg>
   );
-
-  return (
+  return link ? (
+    <a
+      onClick={(e) => disabled && e.preventDefault()}
+      href={link}
+      role="button"
+      draggable="false"
+      className={`govuk-button lbh-button${secondaryClassList + disabledClassList}`}
+      data-module="govuk-button"
+    >
+      {children}
+    </a>
+  ) : (
     <button
       type="button"
       className={`govuk-button lbh-button${secondaryClassList + disabledClassList + addItemClassList}`}
--- components/HackneyDS/index.js
+++ components/HackneyDS/index.js
@@ -1,7 +1,5 @@
 import Input from './Input';
 import Button from './Button';
-// import Radio from './Radio';
-// import Fieldset from './Fieldset';
 import Accordion from './Accordion';
 import Announcement from './Announcement';
 import BackLink from './BackLink';
@@ -17,8 +15,6 @@ import { Tab } from './Tabs/Tab';
 export {
   Input,
   Button,
-  // Radio,
-  // Fieldset,
   Accordion,
   Announcement,
   BackLink,
