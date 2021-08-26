01928ec76b508fbca867e70c66aead83a8531d2c
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-08-26 14:16:27 +0100

    hotfix - fixes tab component issue on small screens

--- components/HackneyDS/Tabs/Tab.js
+++ components/HackneyDS/Tabs/Tab.js
@@ -4,10 +4,10 @@ export const Tab = ({ children, activeTab, id }) => {
     const active = activeTab.toString();
     const newId = id.toString();
     const isVisible = active === newId ? '' : '--hidden';
-    return (
+    return isVisible ? (
       <section className={`govuk-tabs__panel${isVisible}`} id="past-day">
         {children}
       </section>
-    );
+    ) : null;
   };
   
