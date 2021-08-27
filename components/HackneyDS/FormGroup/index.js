e0e52f801ff1f58a1b9b15e9d8a82c4ee8758eef
Vadim Kabaev <v.kabaev@dunice.net>
2021-08-27 18:16:36 +0300

    add the form group decorator

---
+++ components/HackneyDS/FormGroup/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function InputGroup({ children = [], title, hint, error }) {
+  const nodeList = Array.isArray(children) ? children : [children];
+  const errorClassList = error ? ' govuk-form-group--error' : '';
+
+  return (
+    <div className={`govuk-form-group lbh-form-group${errorClassList}`}>
+      <fieldset className="govuk-fieldset lbh-fieldset">
+        <legend className="govuk-fieldset__legend">{title}</legend>
+        <span className="govuk-hint lbh-hint">{hint}</span>
+        <span className="govuk-error-message">
+          <span className="govuk-visually-hidden">Error:</span>
+          {error}
+        </span>
+        {nodeList}
+      </fieldset>
+    </div>
+  );
+}
