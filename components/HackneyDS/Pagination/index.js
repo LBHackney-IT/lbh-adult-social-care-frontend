69465a99f45cd6fe38992894190ae58afc850fe8
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-08-31 15:22:10 +0100

    Adds Pagination component

---
+++ components/HackneyDS/Pagination/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+export const Pagination = ({ totalCount, currentPage, pageSize, onPageChange, siblingCount }) => {
+  const [activePage, setActivePage] = useState(currentPage);
+  const firstResultOnPage = activePage * pageSize - pageSize + 1;
+  const lastResultOnPage = activePage * pageSize;
+  const pageCount = Math.ceil(totalCount / pageSize);
+  const pageArray = Array.from(Array(pageCount));
+
+  const handlePageChange = (newPage) => {
+    if (newPage !== 0 && newPage <= pageCount) {
+      setActivePage(newPage);
+      if (onPageChange) onPageChange(newPage);
+    }
+  };
+
+  return (
+    <nav className="lbh-pagination">
+      <div className="lbh-pagination__summary">{`Showing ${firstResultOnPage}—${lastResultOnPage} of ${totalCount} results`}</div>
+      <ul className="lbh-pagination__list">
+        <li
+          role="button"
+          className={`lbh-pagination__item${activePage - 1 === 0 ? '--disabled' : ''}`}
+          onClick={() => handlePageChange(activePage - 1)}
+        >
+          <a className="lbh-pagination__link" aria-label="Previous page">
+            <span aria-hidden="true" role="presentation">
+              &laquo;
+            </span>
+            Previous
+          </a>
+        </li>
+        {pageArray.map((p, index) => {
+          if (index + 1 >= activePage - siblingCount && index + 1 <= activePage + siblingCount)
+            return (
+              <li
+                key={index}
+                role="button"
+                className="lbh-pagination__item"
+                onClick={() => handlePageChange(index + 1)}
+              >
+                <a className={`lbh-pagination__link${activePage === index + 1 ? '--current' : ''}`} aria-label="Page 1">
+                  {index + 1}
+                </a>
+              </li>
+            );
+          return null;
+        })}
+        <li
+          className={`lbh-pagination__item${activePage === pageCount ? '--disabled' : ''}`}
+          onClick={() => handlePageChange(activePage + 1)}
+          role="button"
+        >
+          <a className="lbh-pagination__link" aria-label="Next page">
+            Next
+            <span aria-hidden="true" role="presentation">
+              &raquo;
+            </span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
