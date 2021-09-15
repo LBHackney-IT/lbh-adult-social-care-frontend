64d778503520921cb22fe10be4c1d3a53c8892bd
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-09-15 12:17:07 +0300

    fix label

--- components/HackneyDS/lettering/Label/index.js
+++ components/HackneyDS/lettering/Label/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default function Label({ children, className = '', htmlFor }) {
   const outerClass = className ? ` ${className}` : '';
-  return <label  htmlFor={htmlFor} className={`govuk-label lbh-label${outerClass}`}>{children}</label>;
+  return <label htmlFor={htmlFor} className={`govuk-label lbh-label${outerClass}`}>{children}</label>;
 }
