ef887370b6e7d7746c8a45d249a93dfc2634f997
mugnivenko <77606518+mugnivenko@users.noreply.github.com>
2021-09-15 12:03:22 +0300

    Concept/tip (#109)
    * add tip
    * remove unused props
    
    Co-authored-by: Pavel Petrunkin <p.petrunkin@dunice.net>

--- components/ReactTooltipNextJs.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import dynamic from "next/dynamic";
-
-const ReactTooltipNextJs = dynamic(() => import("react-tooltip"), {
-  ssr: false,
-});
-
-export default ReactTooltipNextJs;
