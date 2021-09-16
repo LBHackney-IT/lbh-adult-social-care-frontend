5f5371a2627e6dab95325ef9c611d6668c5d37d2
mugnivenko <77606518+mugnivenko@users.noreply.github.com>
2021-09-16 13:30:37 +0300

    Concept/radio group (#115)
    * add RadioItem, RadioGroup
    
    * fix label
    
    * fix inputs, add horizontal separator
    
    Co-authored-by: Pavel Petrunkin <p.petrunkin@dunice.net>

--- components/HackneyDS/index.js
+++ components/HackneyDS/index.js
@@ -31,6 +31,7 @@ import { IndeterminateCheckbox } from './Table/IndeterminateCheckbox';
 import Header from './Header';
 import InsetText from './InsetText';
 import WarningText from './WarningText';
+import RadioGroup from './RadioGroup';
 import { SimplePagination } from './SimplePagination';
 import { SummaryList } from './SummaryList';
 
@@ -40,6 +41,7 @@ export const getMultipleSlot = (nodeList = [], name) => nodeList.filter((el) => 
 
 export {
   Tip,
+  RadioGroup,
   RadioItem,
   SimplePagination,
   Conversation,
