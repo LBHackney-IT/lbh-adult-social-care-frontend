c8041f1f48ad8ff3a2b64fff78c01902ba00bae8
mugnivenko <77606518+mugnivenko@users.noreply.github.com>
2021-09-30 14:17:41 +0300

    Feature/funded nursing care (#126)
    * add funded-nursing-care
    
    * fix dropzone text
    
    * fix FormGroup
    
    Co-authored-by: Pavel Petrunkin <p.petrunkin@dunice.net>

--- components/HackneyDS/index.js
+++ components/HackneyDS/index.js
@@ -38,6 +38,7 @@ import { SimplePagination } from './SimplePagination';
 import { SummaryList } from './SummaryList';
 import { FileUpload } from './FileUpload/FileUpload';
 import { EditElementModal } from '../CareCharges/EditElementModal';
+import { FundedNursingCare } from '../CarePackages/FundedNursingCare/FundedNursingCare';
 import { AddElementModal } from '../CareCharges/AddElementModal';
 
 export const getSlot = (nodeList = [], name) => nodeList.find((el) => el.props.slot === name);
@@ -86,4 +87,5 @@ export {
   Container,
   EditElementModal,
   AddElementModal,
+  FundedNursingCare,
 };
