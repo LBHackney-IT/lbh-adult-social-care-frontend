1d5f167113834591b5f82a07053ee2b9dc56aad1
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-10-06 11:04:31 +0300

    add supplier-look-up

--- components/Brokerage/SupplierLookUp/index.js
+++ components/Brokerage/SupplierLookUp/index.js
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import BrokerageHeader from '../BrokerageHeader/BrokerageHeader';
 import { Button, Container } from '../../HackneyDS';
 import BrokeragePackageDates from '../BrokeragePackageDates';
 import BrokerageSearchSupplier from './BrokerageSearchSupplier';
-import SupplierLookUpSelected from './SupplierLookUpSelected';
 import BrokerageContainerHeader from '../BrokerageContainerHeader';
 
 export const SupplierLookUp = ({ searchResults }) => {
   const [isOngoing, setIsOngoing] = useState(false);
   const [selectedItem, setSelectedItem] = useState('');
-  const [supplierWeeklyCost, setSupplierWeeklyCost] = useState(0);
-  const [initialNeed] = useState({
-    cost: 0,
-    dates: {
-      dateFrom: null,
-      dateTo: null
-    },
-    isOngoing: false,
-  });
-
-  const [weeklyNeeds, setWeeklyNeeds] = useState([{ ...initialNeed }]);
-  const [oneOffNeeds, setOneOffNeeds] = useState([{ ...initialNeed }]);
-  const [weeklyTotalCost, setWeeklyTotalCost] = useState(0);
-  const [oneOfTotalCost, setOneOfTotalCost] = useState(0);
 
   const [packageDates, setPackageDates] = useState({
     dateFrom: null,
@@ -37,54 +22,11 @@ export const SupplierLookUp = ({ searchResults }) => {
     alert('Click save');
   };
 
-  const changeNeed = (getter, setter, field, value, index) => {
-    const cloneNeed = { ...getter[index] };
-    if (field === 'cost') {
-      cloneNeed.cost = value;
-    } else if (field === 'isOngoing') {
-      cloneNeed.isOngoing = value;
-    } else {
-      cloneNeed.dates = { ...cloneNeed.dates, [field]: value };
-    }
-    const cloneNeeds = getter.slice();
-    cloneNeeds.splice(index, 1, cloneNeed);
-    setter(cloneNeeds);
-  };
-
-  const addNeed = (setter) => {
-    setter(prevState => ([
-      ...prevState,
-      { ...initialNeed },
-    ]));
-  };
-
-  const removeNeed = (getter, setter, index) => {
-    const copyGetter = getter.slice();
-    copyGetter.splice(index, 1);
-    setter(copyGetter);
-  };
-
-  useEffect(() => {
-    let totalCost = Number(supplierWeeklyCost);
-    if (weeklyNeeds) {
-      weeklyNeeds.forEach(item => totalCost += Number(item.cost));
-    }
-    setWeeklyTotalCost(totalCost);
-  }, [supplierWeeklyCost, weeklyNeeds]);
-
-  useEffect(() => {
-    let totalCost = Number(supplierWeeklyCost);
-    if (oneOffNeeds) {
-      oneOffNeeds.forEach(item => totalCost += Number(item.cost));
-    }
-    setOneOfTotalCost(totalCost);
-  }, [supplierWeeklyCost, oneOffNeeds]);
-
   return (
     <div className="supplier-look-up brokerage">
       <BrokerageHeader/>
       <Container className="brokerage__container-main">
-        <BrokerageContainerHeader title='Broker package' />
+        <BrokerageContainerHeader title="Broker package"/>
         <Container>
           <h3 className="brokerage__item-title">Nursing Care</h3>
           <BrokeragePackageDates
@@ -95,25 +37,7 @@ export const SupplierLookUp = ({ searchResults }) => {
             setIsOngoing={setIsOngoing}
           />
         </Container>
-        {
-          selectedItem ?
-            <SupplierLookUpSelected
-              addNeed={addNeed}
-              weeklyNeeds={weeklyNeeds}
-              oneOffNeeds={oneOffNeeds}
-              setWeeklyNeeds={setWeeklyNeeds}
-              setOneOffNeeds={setOneOffNeeds}
-              oneOffTotalCost={oneOfTotalCost}
-              weeklyTotalCost={weeklyTotalCost}
-              supplierWeeklyCost={supplierWeeklyCost}
-              setSupplierWeeklyCost={setSupplierWeeklyCost}
-              changeNeed={changeNeed}
-              removeNeed={removeNeed}
-              setSelectedItem={setSelectedItem}
-              cardInfo={selectedItem}
-            />
-            : <BrokerageSearchSupplier searchResults={searchResults} setSelectedItem={setSelectedItem}/>
-        }
+        <BrokerageSearchSupplier searchResults={searchResults} setSelectedItem={setSelectedItem}/>
         <Container className="brokerage__actions">
           <Button handler={clickBack} className="brokerage__back-button">Back</Button>
           <Button handler={clickSave}>Save and continue</Button>
