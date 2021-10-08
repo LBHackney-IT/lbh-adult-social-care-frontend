c66a4aaa03358dea7d927e9b0464f87dd94cbaeb
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-10-08 17:35:59 +0300

    Don't fetch package if packageId is undefined

--- api/SWR/CarePackage/useReclaimApi.js
+++ api/SWR/CarePackage/useReclaimApi.js
@@ -3,8 +3,10 @@ import useGetData from '../useGetData';
 const CARE_PACKAGES_URL = '/care-packages';
 
 const useReclaimApi = {
-  fnc: (carePackageId) => useGetData(`${CARE_PACKAGES_URL}/${carePackageId}/reclaims/fnc`),
-  activeFncPrice: (carePackageId) =>useGetData(`${CARE_PACKAGES_URL}/${carePackageId}/reclaims/fnc/active-price`),
-}
+  fnc: (carePackageId) =>
+    useGetData(carePackageId !== undefined ? `${CARE_PACKAGES_URL}/${carePackageId}/reclaims/fnc` : null),
+  activeFncPrice: (carePackageId) =>
+    useGetData(carePackageId !== undefined ? `${CARE_PACKAGES_URL}/${carePackageId}/reclaims/fnc/active-price` : null),
+};
 
 export default useReclaimApi;
--- pages/care-package/brokerage/funded-nursing-care/[id].js
+++ pages/care-package/brokerage/funded-nursing-care/[id].js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import FundedNursingCare from 'components/Brokerage/FundedNursingCare';
 import { useRouter } from 'next/router';
 import { useDispatch } from 'react-redux';
-import {
-  createCarePackageReclaimFnc,
-  updateCarePackageReclaimFnc
-} from 'api/CarePackages/CarePackageReclaim';
+import { createCarePackageReclaimFnc, updateCarePackageReclaimFnc } from 'api/CarePackages/CarePackageReclaim';
 import useReclaimApi from 'api/SWR/CarePackage/useReclaimApi';
 import { addNotification } from 'reducers/notificationsReducer';
 
@@ -17,16 +14,17 @@ const FundedNursingCarePage = () => {
   const { data: activeFncPrice } = useReclaimApi.activeFncPrice(carePackageId);
 
   const collectedByOptions = [
+    { text: 'Select One', value: null },
     { text: 'Supplier', value: '1' },
     { text: 'Hackney', value: '2' },
   ];
-  
+
   const pushNotification = (text, className = 'error') => {
     dispatch(addNotification({ text, className }));
   };
 
-  const createFundedNursingCare = (carePackageId, fundedNursingCareCreation) => {
-    createCarePackageReclaimFnc(carePackageId, fundedNursingCareCreation)
+  const createFundedNursingCare = (packageId, fundedNursingCareCreation) => {
+    createCarePackageReclaimFnc(packageId, fundedNursingCareCreation)
       .then(() => {
         pushNotification(`Funded Nursing Care created successfully`, 'success');
       })
@@ -35,8 +33,8 @@ const FundedNursingCarePage = () => {
       });
   };
 
-  const updateFundedNursingCare = (carePackageId, fundedNursingCareUpdate) => {
-    updateCarePackageReclaimFnc(carePackageId, fundedNursingCareUpdate)
+  const updateFundedNursingCare = (packageId, fundedNursingCareUpdate) => {
+    updateCarePackageReclaimFnc(packageId, fundedNursingCareUpdate)
       .then(() => {
         pushNotification(`Funded Nursing Care updated successfully`, 'success');
       })
@@ -45,17 +43,18 @@ const FundedNursingCarePage = () => {
       });
   };
 
-  useEffect(() => {
-  });
+  useEffect(() => {});
 
-  return <FundedNursingCare
-          carePackageId={carePackageId}
-          collectedByOptions={collectedByOptions}
-          activeFncPrice={activeFncPrice}
-          carePackageReclaimFnc={carePackageReclaimFnc}
-          createFundedNursingCare={createFundedNursingCare}
-          updateFundedNursingCare={updateFundedNursingCare}
-          />;
+  return (
+    <FundedNursingCare
+      carePackageId={carePackageId}
+      collectedByOptions={collectedByOptions}
+      activeFncPrice={activeFncPrice}
+      carePackageReclaimFnc={carePackageReclaimFnc}
+      createFundedNursingCare={createFundedNursingCare}
+      updateFundedNursingCare={updateFundedNursingCare}
+    />
+  );
 };
 
 export default FundedNursingCarePage;
