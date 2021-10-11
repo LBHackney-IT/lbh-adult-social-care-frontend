c416f6235c2958e472aeb53d90db6749e79dcf39
Vadim Kirpun <quinze105@gmail.com>
2021-10-11 17:18:05 +0300

    update BrokerageHub page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { useGetData } from 'api/SWR';
 import DatePick from 'components/DatePick';
 import FormGroup from 'components/HackneyDS/FormGroup';
 import Pagination from 'components/Payments/Pagination';
@@ -8,10 +7,16 @@ import BrokerageHeader from 'components/Brokerage/BrokerageHeader/BrokerageHeade
 import { BrokerageHubTable } from 'components/Brokerage/BrokerageHub/BrokerageHubTable';
 import { Button, Container, HorizontalSeparator, SearchBox, Select } from 'components/HackneyDS';
 import { CARE_PACKAGE_ROUTE, APPROVER_HUB_ROUTE, BROKERAGE_HUB_ROUTE, LOGOUT_ROUTE } from 'routes/RouteConstants';
+import useCarePackageApi from '../api/SWR/CarePackage/useCarePackageApi';
 
 const statusOptions = [
-  { text: 'Status-1', value: 'status-1' },
-  { text: 'Status-2', value: 'status-2' },
+  { text: 'New', value: '1' },
+  { text: 'In Progress', value: '2' },
+  { text: 'Submitted For Approval', value: '3' },
+  { text: 'Approved', value: '4' },
+  { text: 'Not Approved', value: '5' },
+  { text: 'Ended', value: '6' },
+  { text: 'Cancelled', value: '7' },
 ];
 
 const links = [
@@ -22,10 +27,11 @@ const links = [
 ];
 
 const BrokerageHub = () => {
-  const { data } = useGetData(`care-packages/broker-view`);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { data } = useCarePackageApi.brokerView({ pageNumber });
 
   const router = useRouter();
-  const [currentPage, setCurrentPage] = useState(1);
   const [status, setStatus] = useState('');
   const [searchPackages, setSearchPackages] = useState('');
   const [dateFrom, setDateFrom] = useState(null);
@@ -33,8 +39,8 @@ const BrokerageHub = () => {
 
   const onRowClick = (rowItem) => {
     router.push({
-      pathname: `${router.pathname}/${rowItem.packageId}`,
-      query: rowItem,
+      pathname: `care-package/service-users/${rowItem?.serviceUserId}/core-package-details`,
+      query: { packageId: rowItem.packageId },
     });
   };
 
@@ -47,48 +53,56 @@ const BrokerageHub = () => {
       <BrokerageHeader links={links} />
 
       <Container background="#FAFAFA" padding="60px 60px 30px 60px">
-        <Container className="brokerage-hub__header">
-          <h2>Broker Portal</h2>
-          <Button handler={findServiceUser}>Find a service user</Button>
-        </Container>
-        <Container className="brokerage-hub__filters">
-          <SearchBox placeholder="Search" label="Search packages" handler={setSearchPackages} value={searchPackages} />
-          <FormGroup className="form-group--inline-label brokerage-hub__form-status" label="Status">
-            <Select options={statusOptions} value={status} onChange={({ target: { value } }) => setStatus(value)} />
-          </FormGroup>
-          <FormGroup className="form-group--inline-label brokerage-hub__date-from" label="From">
-            <DatePick
-              placeholder="Select date"
-              startDate={dateFrom}
-              dateValue={dateFrom}
-              setDate={(value) => {
-                if (value > dateTo) {
-                  setDateTo(value);
-                }
-                setDateFrom(value);
-              }}
+        <Container maxWidth="1080px" margin="0 auto" padding="30px 60px">
+          <Container className="brokerage-hub__header">
+            <h2>Broker Portal</h2>
+            <Button handler={findServiceUser}>Find a service user</Button>
+          </Container>
+          <Container className="brokerage-hub__filters">
+            <SearchBox
+              placeholder="Search"
+              label="Search packages"
+              handler={setSearchPackages}
+              value={searchPackages}
             />
-          </FormGroup>
-          <FormGroup className="form-group--inline-label" label="To">
-            <DatePick
-              placeholder="Select date"
-              startDate={dateTo}
-              dateValue={dateTo}
-              minDate={dateFrom}
-              setDate={setDateTo}
-            />
-          </FormGroup>
+            <FormGroup className="form-group--inline-label brokerage-hub__form-status" label="Status">
+              <Select options={statusOptions} value={status} onChange={({ target: { value } }) => setStatus(value)} />
+            </FormGroup>
+            <FormGroup className="form-group--inline-label brokerage-hub__date-from" label="From">
+              <DatePick
+                placeholder="Select date"
+                startDate={dateFrom}
+                dateValue={dateFrom}
+                setDate={(value) => {
+                  if (value > dateTo) {
+                    setDateTo(value);
+                  }
+                  setDateFrom(value);
+                }}
+              />
+            </FormGroup>
+            <FormGroup className="form-group--inline-label" label="To">
+              <DatePick
+                placeholder="Select date"
+                startDate={dateTo}
+                dateValue={dateTo}
+                minDate={dateFrom}
+                setDate={setDateTo}
+              />
+            </FormGroup>
+          </Container>
         </Container>
       </Container>
-      <Container padding="30px 60px 60px 60px">
+
+      <Container maxWidth="1080px" margin="0 auto" padding="30px 60px 60px 60px">
         {data?.packages && <BrokerageHubTable onRowClick={onRowClick} data={data.packages} />}
         <HorizontalSeparator height="20px" />
         <Pagination
           pageSize={data?.pagingMetaData?.pageSize ?? 0}
           totalPages={data?.pagingMetaData?.totalPages ?? 0}
-          currentPage={currentPage}
           totalCount={data?.pagingMetaData?.totalCount ?? 0}
-          changePagination={setCurrentPage}
+          currentPage={pageNumber}
+          changePagination={setPageNumber}
         />
       </Container>
     </div>
