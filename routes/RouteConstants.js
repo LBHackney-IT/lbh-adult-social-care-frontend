8cbc913af12ad512816e68bb5bd902a9a25827a0
Vadim Kirpun <quinze105@gmail.com>
2021-10-13 14:21:25 +0300

    clean Routes file

--- routes/RouteConstants.js
+++ routes/RouteConstants.js
@@ -1,4 +1,3 @@
-export const LOGOUT_ROUTE = '/logout';
 export const BROKER_PORTAL_ROUTE = '/broker-portal';
 
 export const CARE_PACKAGE_ROUTE = '/care-package';
@@ -8,9 +7,4 @@ export const getBrokerPackageRoute = (id) => `${CARE_PACKAGE_ROUTE}/${id}/broker
 export const getFundedNursingCareRoute = (id) => `${CARE_PACKAGE_ROUTE}/${id}/broker-fnc`;
 export const getCareChargesRoute = (id) => `${CARE_PACKAGE_ROUTE}/${id}/broker-care-charges`;
 
-export const BROKERAGE_ROUTE = `${CARE_PACKAGE_ROUTE}/brokerage`;
-export const BROKER_PACKAGE_ROUTE = `${BROKERAGE_ROUTE}/broker-package`;
-export const PAYMENTS_ROUTE = '/payments';
-export const PAYMENTS_PAY_RUNS_ROUTE = `${PAYMENTS_ROUTE}/pay-runs`;
-export const PAYMENTS_BILLS_ROUTE = `${PAYMENTS_ROUTE}/bills`;
-
+export const LOGOUT_ROUTE = '/logout';
