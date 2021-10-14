0ba519f8d585c20e4c50dda6e8f38ffb70b68d22
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-10-14 12:31:17 +0300

    fix home link

--- components/Brokerage/PackageDetails/index.js
+++ components/Brokerage/PackageDetails/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { BROKER_PORTAL_ROUTE, CARE_PACKAGE_ROUTE, getBrokerPackageRoute } from '../../../routes/RouteConstants';
+import { BROKER_PORTAL_ROUTE, getBrokerPackageRoute } from '../../../routes/RouteConstants';
 import BrokerageHeader from '../BrokerageHeader/BrokerageHeader';
 import { Button, Container, Link, Breadcrumbs } from '../../HackneyDS';
 import PackageUserDetails from '../PackageUserDetails';
@@ -20,7 +20,7 @@ const links = [
 ];
 
 const breadcrumbs = [
-  { text: 'Home', href:  CARE_PACKAGE_ROUTE },
+  { text: 'Home', href:  '/' },
   { text: 'Broker Portal', href: BROKER_PORTAL_ROUTE },
   { text: 'Full overview' },
 ];
