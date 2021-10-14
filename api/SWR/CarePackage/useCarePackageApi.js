66c341696186ad245341781a7c23d3e515db67d7
Vadim Kirpun <quinze105@gmail.com>
2021-10-14 11:07:56 +0300

    update core package page routing

--- api/SWR/CarePackage/useCarePackageApi.js
+++ api/SWR/CarePackage/useCarePackageApi.js
@@ -6,16 +6,27 @@ const CARE_PACKAGES_URL = '/care-packages';
 const useCarePackageApi = {
   summary: (carePackageId) =>
     useGetData(carePackageId !== undefined ? `${CARE_PACKAGES_URL}/${carePackageId}/summary` : null),
-  brokerView: ({ pageNumber, toDate, fromDate, status, serviceUserId }) => (
-    useGetData(`${CARE_PACKAGES_URL}/broker-view${getQueryParamsFromObject({ pageNumber, status, toDate, fromDate, serviceUserId })}`)
-  ),
+
+  brokerView: ({ pageNumber, toDate, fromDate, status, serviceUserId }) =>
+    useGetData(
+      `${CARE_PACKAGES_URL}/broker-view${getQueryParamsFromObject({
+        pageNumber,
+        status,
+        toDate,
+        fromDate,
+        serviceUserId,
+      })}`
+    ),
+
   coreSettings: (carePackageId) =>
     useGetData(
       carePackageId !== undefined || carePackageId !== 'undefined' ? `${CARE_PACKAGES_URL}/${carePackageId}/core` : null
     ),
+
   details: (packageId) => useGetData(packageId !== undefined ? `${CARE_PACKAGES_URL}/${packageId}/details` : null),
   suppliers: ({ supplierName }) => useGetData(`/suppliers${getQueryParamsFromObject({ supplierName })}`),
   singleSupplier: (supplierId) => useGetData(supplierId !== undefined ? `/suppliers/${supplierId}` : null),
+  singlePackageInfo: (packageId) => useGetData(packageId ? `${CARE_PACKAGES_URL}/${packageId}` : null),
 };
 
 export default useCarePackageApi;
