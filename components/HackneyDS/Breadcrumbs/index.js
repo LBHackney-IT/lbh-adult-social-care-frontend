1b062e4c39b7da7508f6e8d1b93d73340c2b93d5
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-10-14 19:09:37 +0300

    fix breadcrumbs

--- components/HackneyDS/Breadcrumbs/index.js
+++ components/HackneyDS/Breadcrumbs/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { BreadcrumbsChevron } from '../../Icons';
 
-export default function Breadcrumbs({ values }) {
+export default function Breadcrumbs ({ values }) {
   return (
     <div className="govuk-breadcrumbs lbh-breadcrumbs">
       <ol className="govuk-breadcrumbs__list">
-        {values.map((item) => (
-          <li className="govuk-breadcrumbs__list-item" key={Math.random()}>
-            <span
-              className="govuk-breadcrumbs__link govuk-breadcrumbs__link--available"
-              onClick={(e) => {
-                e.preventDefault();
-                item.onClick();
-              }}
-            >
-              {item.text}
-            </span>
-          </li>
-        ))}
+        {values.map(({ text, href }, index) => {
+          const isLastIndex = index + 1 === values.length;
+          return (
+            <li className="govuk-breadcrumbs__list-item" key={Math.random()}>
+              {href ? (
+                <a className="govuk-breadcrumbs__link govuk-breadcrumbs__link--available" href={href}>
+                  {text}
+                </a>
+              ) : <p className="govuk-breadcrumbs__default-text">{text}</p>
+              }
+              {!isLastIndex && <BreadcrumbsChevron/>}
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
