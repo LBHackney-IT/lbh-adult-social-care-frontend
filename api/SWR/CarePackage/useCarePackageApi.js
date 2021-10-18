6b835cdb5f7c2fc48cdb0128473a3dbd0a7c56f2
Vadim Kirpun <quinze105@gmail.com>
2021-10-18 16:40:32 +0300

    add the endpoint for history

--- api/SWR/CarePackage/useCarePackageApi.js
+++ api/SWR/CarePackage/useCarePackageApi.js
@@ -28,7 +28,8 @@ const useCarePackageApi = {
     useGetData(shouldFetch ? `/suppliers${getQueryParamsFromObject({ supplierName })}` : null),
 
   singleSupplier: (supplierId) => useGetData(supplierId ? `/suppliers/${supplierId}` : null, '', {}),
-  singlePackageInfo: (packageId) => useGetData(`${CARE_PACKAGES_URL}/${packageId}`, '', {}),
+  singlePackageInfo: (packageId) => useGetData(packageId ? `${CARE_PACKAGES_URL}/${packageId}` : null, '', {}),
+  history: (packageId) => useGetData(packageId ? `${CARE_PACKAGES_URL}/${packageId}/history` : null, '', {}),
 };
 
 export default useCarePackageApi;
