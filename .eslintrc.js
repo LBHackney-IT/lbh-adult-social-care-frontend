4744fba79b65cacaf312dee09015e3955a47b0d9
Vadim Kirpun <quinze105@gmail.com>
2021-10-21 15:24:08 +0300

    setup lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
-  extends: ['plugin:@next/next/recommended', 'airbnb', 'prettier'],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  env: {
+    browser: true,
+    es2021: true,
   },
+  extends: ['airbnb', 'plugin:@next/next/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  plugins: ['react'],
   settings: {
     'import/resolver': {
       alias: {
