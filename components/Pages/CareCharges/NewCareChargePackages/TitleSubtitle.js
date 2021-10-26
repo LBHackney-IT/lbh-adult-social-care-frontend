43451454207fdb644f4fe1276e6a9cef729b8a16
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-10-26 16:35:11 +0300

    fix outline button hover

--- components/Pages/CareCharges/NewCareChargePackages/TitleSubtitle.js
+++ components/Pages/CareCharges/NewCareChargePackages/TitleSubtitle.js
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { Container } from '../../../HackneyDS';
 
 const TitleSubtitle = ({ title, subtitle, className = '' }) => (
-  <Container width='fit-content' className={`title-subtitle ${className}`}>
+  <Container className={`title-subtitle ${className}`}>
     <p className='font-size-14px'>{title}</p>
     <p className='font-size-16px'>{subtitle}</p>
   </Container>
