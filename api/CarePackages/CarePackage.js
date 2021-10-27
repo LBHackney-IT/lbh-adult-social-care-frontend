3c2b5210efd154bcdd1cd28e94b04641bfa82bec
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-10-27 09:48:19 +0300

    add approval page, refactoring

--- api/CarePackages/CarePackage.js
+++ api/CarePackages/CarePackage.js
@@ -8,24 +8,24 @@ const sendCarePackageRequest = (url = '', data, method = requestMethods.post) =>
   axiosRequest({ url: `${CARE_PACKAGE_URL}${url}`, data, method });
 
 export const changeCarePackageDetails = ({ data, packageId }) =>
-  sendCarePackageRequest(`/${packageId}/details`, data, requestMethods.put)
+  sendCarePackageRequest(`/${packageId}/details`, data, requestMethods.put);
 
-export const submitCarePackage = ({ data, packageId }) => sendCarePackageRequest(`/${packageId}/submit`, data)
+export const submitCarePackage = ({ data, packageId }) => sendCarePackageRequest(`/${packageId}/submit`, data);
 
-export const approveCarePackage = ({ data, packageId }) => sendCarePackageRequest(`/${packageId}/approve`, data)
+export const approveCarePackage = (packageId, notes) => sendCarePackageRequest(`/${packageId}/approve`, { notes });
 
-export const declineCarePackage = ({ data, packageId }) => sendCarePackageRequest(`/${packageId}/decline`, data)
+export const declineCarePackage = (packageId, notes) => sendCarePackageRequest(`/${packageId}/decline`, { notes });
 
-export const endCarePackage = (packageId, notes) => sendCarePackageRequest(`/${packageId}/end`, { notes })
+export const endCarePackage = (packageId, notes) => sendCarePackageRequest(`/${packageId}/end`, { notes });
 
-export const cancelCarePackage = (packageId, notes) => sendCarePackageRequest(`/${packageId}/cancel`, { notes })
+export const cancelCarePackage = (packageId, notes) => sendCarePackageRequest(`/${packageId}/cancel`, { notes });
 
-export const createCoreCarePackage = ({ data }) => sendCarePackageRequest('', data)
+export const createCoreCarePackage = ({ data }) => sendCarePackageRequest('', data);
 
 export const updateCoreCarePackage = ({ data, packageId }) =>
-  sendCarePackageRequest(`/${packageId}`, data, requestMethods.put)
+  sendCarePackageRequest(`/${packageId}`, data, requestMethods.put);
 
 export const updateCarePackageCosts = ({ data, packageId }) =>
-  sendCarePackageRequest(`/${packageId}/details`, data, requestMethods.put)
+  sendCarePackageRequest(`/${packageId}/details`, data, requestMethods.put);
 
-export const assignToBroker = ({ data }) => sendCarePackageRequest('/assign', data)
+export const assignToBroker = ({ data }) => sendCarePackageRequest('/assign', data);
--- components/Pages/BrokerPortal/ActionCarePackageModal.js
+++ components/Pages/BrokerPortal/ActionCarePackageModal.js
@@ -4,7 +4,7 @@ import { Button, Container, Dialog, Textarea, FormGroup } from '../../HackneyDS'
 const ActionCarePackageModal = ({ close, title, actions, isOpened, notes, setNotes, className }) => (
   <Dialog isOpen={isOpened} onClose={close} className={className}>
     <h2>{title}</h2>
-    <FormGroup className="brokerage__add-notes" label="Notes">
+    <FormGroup className="brokerage__add-notes" label="Add Notes">
       <Textarea value={notes} handler={setNotes}/>
     </FormGroup>
     <Container className="brokerage__actions">
