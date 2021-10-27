1d788c90318c583463321beabe54b589e6fa6dab
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-10-27 17:33:57 +0300

    fix function

--- api/SWR/package/usePackageGetAll.js
+++ api/SWR/package/usePackageGetAll.js
@@ -2,21 +2,21 @@ import { useMemo } from 'react';
 import useGetData from '../useGetData';
 import { useFetchWithParams } from '../useFetchWithParams';
 
-const usePackageGetAll = () => {
-  const { data, isLoading } = useGetData('/lookups?name=packageType', 'Can not get packages', []);
+export const useLookups = ({ params, url = '', shouldFetch }) => {
+  if(!params) {
+    const { data, isLoading } = useGetData('/lookups?name=packageType', 'Can not get packages', []);
 
-  const options = useMemo(() => data.map((type) => ({ value: type.id, text: type.name })), [data]);
+    const options = useMemo(() => data.map((type) => ({ value: type.id, text: type.name })), [data]);
 
-  return { data, options, isLoading };
-};
-
-export const useGetPackage = ({ params, url = '', shouldFetch }) => (
+    return { data, options, isLoading };
+  }
+  return (
   useFetchWithParams({
     shouldFetch,
     params,
     url: `/lookups${url ? ` ${url}` : ''}`,
     errorText: 'Can not get package'
   })
-);
+)};
 
-export default usePackageGetAll;
+export default useLookups;
