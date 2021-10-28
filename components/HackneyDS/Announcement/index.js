53e02c0efd63620228f4b72f25da589280ac2950
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-10-28 13:22:46 +0100

    Fixes announcement (#246)

--- components/HackneyDS/Announcement/index.js
+++ components/HackneyDS/Announcement/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 
 const mainClass = 'lbh-announcement';
 
-export default function Announcement({ title, children, className, isWarning, isError }) {
-
-  const isWarningClass = isWarning ? ` ${mainClass}--warning` : '';
+export default function Announcement({ title, children, className, isError }) {
   const isErrorClass = isError ? ` ${mainClass}--error` : '';
   return (
-    <section
-      className={`${mainClass} ${mainClass}--site${className && ` ${className}`}${isWarningClass}${isErrorClass}`}
-    >
+    <section className={`${mainClass} ${mainClass}--site${className && ` ${className}`}${isErrorClass}`}>
       <h3 className={`${mainClass}__title`}>{title}</h3>
       <div className={`${mainClass}__content`}>{children}</div>
     </section>
