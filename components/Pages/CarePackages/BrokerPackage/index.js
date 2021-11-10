bc54df997c80f26ff046201606f81bde964983d1
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-11-10 15:34:26 +0300

    fix autosearch after remove selected supplier

--- components/Pages/CarePackages/BrokerPackage/index.js
+++ components/Pages/CarePackages/BrokerPackage/index.js
@@ -442,6 +442,7 @@ const BrokerPackage = ({
               setSelectedItem={(value) => {
                 setSelectedItem(value);
                 setShowSearchResults(false);
+                setSearchQuery('');
               }}
             />
           ) : (
