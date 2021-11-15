115795d9d0ac90c211921a782a2a5fceccb5d0b9
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-11-15 12:14:24 +0300

    refactoring

--- components/Pages/Finance/CreateDraftPayRun/CreatePayRunInfo.js
+++ components/Pages/Finance/CreateDraftPayRun/CreatePayRunInfo.js
@@ -1,6 +1,7 @@
 import { Controller } from 'react-hook-form';
 import React from 'react';
-import { Container, DatePicker, FormGroup, RadioGroup } from '../../../HackneyDS';
+import { Container, RadioGroup } from '../../../HackneyDS';
+import CreatePayRunDates from './CreatePayRunDates';
 
 const CreatePayRunInfo = ({
   errors,
@@ -38,50 +39,18 @@ const CreatePayRunInfo = ({
       />
     </Container>
     <h4>Payrun scheduling</h4>
-    <Container className="create-pay-run__date-to">
-      <Controller
-        name={fieldStart}
-        control={control}
-        render={({ field }) => (
-          <FormGroup error={errors[fieldStart]?.message}>
-            <DatePicker
-              date={field.value}
-              setDate={(value) => {
-                onChangeDate('startDate', value);
-                field.onChange(value);
-              }}
-              maxDate={maxDate}
-              minDate={minDate}
-              dateValue={field.value}
-              day={{ label: startDateLabel }}
-            />
-            {dateText}
-          </FormGroup>
-        )}
-      />
-      {paidUpToDate !== undefined && (
-        <Controller
-          name="paidUpToDate"
-          control={control}
-          render={({ field }) => (
-            <FormGroup error={errors.paidUpToDate?.message}>
-              <DatePicker
-                date={field.value}
-                setDate={(value) => {
-                  onChangeDate('paidUpToDate', value);
-                  field.onChange(value);
-                }}
-                day={{ label: 'To' }}
-                checkMinDate
-                maxDate={maxDate}
-                minDate={startDate}
-                dateValue={field.value}
-              />
-            </FormGroup>
-          )}
-        />
-      )}
-    </Container>
+    <CreatePayRunDates
+      minDate={minDate}
+      startDate={startDate}
+      startDateLabel={startDateLabel}
+      errors={errors}
+      fieldStart={fieldStart}
+      paidUpToDate={paidUpToDate}
+      maxDate={maxDate}
+      onChangeDate={onChangeDate}
+      dateText={dateText}
+      control={control}
+    />
   </Container>
 );
 
