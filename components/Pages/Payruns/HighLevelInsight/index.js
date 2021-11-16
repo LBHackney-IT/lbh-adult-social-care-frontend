b56cbd27dec8e4927f8dc6f91492431ac3ebe695
Pavel Petrunkin <p.petrunkin@dunice.net>
2021-11-16 18:49:29 +0300

    add react hook form

--- components/Pages/Payruns/HighLevelInsight/index.js
+++ components/Pages/Payruns/HighLevelInsight/index.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Button, Container, Heading, Hint, HorizontalSeparator, VerticalSeparator } from 'components';
 import { getNumberWithCommas } from 'service';
 import { approvePayRun, rejectPayRun } from 'api/PayRun';
 import { useDispatch } from 'react-redux';
 import { addNotification } from 'reducers/notificationsReducer';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { Dialog, FormGroup, Textarea } from '../../../HackneyDS';
 import Loading from '../../../Loading';
+
+const errorText = {
+  Approve: 'approval',
+  Decline: 'decline'
+};
 
 export const HighLevelInsight = ({
   payRunId,
@@ -20,16 +28,22 @@ export const HighLevelInsight = ({
   const dispatch = useDispatch();
   const [openedModal, setOpenedModal] = useState('');
   const [loading, setLoading] = useState(false);
-  const [notesError, setNotesError] = useState('');
-  const [notes, setNotes] = useState('');
   const isDeclineModal = openedModal === 'Decline';
   const isApproveModal = openedModal === 'Approve';
+
+  const schema = useMemo(() => (
+    yup.object().shape({
+      notes: yup
+        .string()
+        .required(`Please put a reason for ${errorText[openedModal]}`)
+        .test('notes', `Please put a reason for ${errorText[openedModal]}`, (value) => value.trim?.())
+    })), [openedModal]);
 
   const pushNotification = (text, className = 'error') => {
     dispatch(addNotification({ text, className }));
   };
 
-  const handleApprove = async () => {
+  const handleApprove = async (notes) => {
     try {
       await approvePayRun(payRunId, notes);
       pushNotification(`Invoice status changed`, 'success');
@@ -45,26 +59,22 @@ export const HighLevelInsight = ({
 
   const closeModal = () => {
     setOpenedModal('');
-    setNotesError('');
-    setNotes('');
+    reset();
   };
 
-  const makePayRunAction = async () => {
-    if (notes.trim() === '') {
-      return setNotesError(`Please put a reason for ${openedModal.toLowerCase()}`);
-    }
-
+  const makePayRunAction = async ({ notes }) => {
+    console.log(notes);
     setLoading(true);
     if (isApproveModal) {
-      await handleApprove();
+      await handleApprove(notes);
     } else {
-      await handleReject();
+      await handleReject(notes);
     }
     closeModal();
     setLoading(false);
   };
 
-  const handleReject = async () => {
+  const handleReject = async (notes) => {
     try {
       await rejectPayRun(payRunId, notes);
       pushNotification(`Invoice status changed`, 'success');
@@ -74,12 +84,18 @@ export const HighLevelInsight = ({
     }
   };
 
-  const onChangeNotes = (value) => {
-    setNotesError('');
-    setNotes(value);
-  };
-
   const increaseOrDecrease = difference > 0 ? 'increase' : 'decrease';
+
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: { notes: '' },
+  });
+  const onSubmit = (data) => makePayRunAction(data);
 
   return (
     <>
@@ -87,29 +103,42 @@ export const HighLevelInsight = ({
       <Dialog className="high-level-insight--dialog" isOpen={openedModal} noBorder closeIcon="" onClose={closeModal}>
         <h3>{openedModal} Pay Run</h3>
         <HorizontalSeparator height={32} />
-        <FormGroup horizontalSeparator={8} error={notesError} required label="Add Notes">
-          <Textarea value={notes} handler={onChangeNotes} rows={5} />
-        </FormGroup>
-        <HorizontalSeparator height={32} />
-        <Container display="flex" alignItems="center">
-          <Button
-            onClick={makePayRunAction}
-            secondary={isDeclineModal}
-            color={isDeclineModal && 'red'}
-          >
-            {openedModal} Pay Run
-          </Button>
-          <VerticalSeparator width={24} />
-          <Button
-            onClick={closeModal}
-            outline
-            color="gray"
-            secondary
-            className="no-border link-button"
-          >
-            Cancel
-          </Button>
-        </Container>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            name="notes"
+            render={({ field }) => (
+              <FormGroup horizontalSeparator={8} error={errors.notes?.message} required label="Add Notes">
+                <Textarea
+                  value={field.value}
+                  handler={field.onChange}
+                  rows={5}
+                  {...field}
+                />
+              </FormGroup>
+            )}
+          />
+          <HorizontalSeparator height={32} />
+          <Container display="flex" alignItems="center">
+            <Button
+              type="submit"
+              secondary={isDeclineModal}
+              color={isDeclineModal && 'red'}
+            >
+              {openedModal} Pay Run
+            </Button>
+            <VerticalSeparator width={24} />
+            <Button
+              onClick={closeModal}
+              outline
+              color="gray"
+              secondary
+              className="no-border link-button"
+            >
+              Cancel
+            </Button>
+          </Container>
+        </form>
       </Dialog>
       <Container background="#FAFAFA" padding="24px 16px">
         <Container display="flex" justifyContent="space-between">
