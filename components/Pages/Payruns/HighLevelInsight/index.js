49bf77ccbf236fa62d85e61ff2d66888fd07d98b
mugnivenko <77606518+mugnivenko@users.noreply.github.com>
2021-11-17 12:49:01 +0300

    Feature/payrun approve decline (#317)
    * fix layout, integration
    
    * fix layout, integration
    
    * fix integration
    
    * fix api, change error message
    
    * add react hook form
    
    * refactoring
    
    * refactoring
    
    * fix header focus
    
    Co-authored-by: Pavel Petrunkin <p.petrunkin@dunice.net>

--- components/Pages/Payruns/HighLevelInsight/index.js
+++ components/Pages/Payruns/HighLevelInsight/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import { Button, Container, Heading, Hint, HorizontalSeparator, VerticalSeparator } from 'components';
+import React, { useState } from 'react';
+import { Button, Container, Heading, Hint, HorizontalSeparator } from 'components';
 import { getNumberWithCommas } from 'service';
-import { approvePayRun } from 'api/PayRun';
-import { useDispatch } from 'react-redux';
-import { addNotification } from 'reducers/notificationsReducer';
+import ApproveDeclineModal from '../ApproveDeclineModal';
 
 export const HighLevelInsight = ({
   payRunId,
@@ -13,69 +11,64 @@ export const HighLevelInsight = ({
   serviceUsers = 0,
   holdCount = 0,
   holdValue = 0,
+  update,
 }) => {
-  const dispatch = useDispatch();
-  const pushNotification = (text, className = 'error') => {
-    dispatch(addNotification({ text, className }));
-  };
-  const params = { notes: 'test' };
+  const [openedModal, setOpenedModal] = useState('');
 
-  const handleApprove = async () => {
-    try {
-      await approvePayRun({ payRunId, params });
-      pushNotification(`Invoice status changed`, 'success');
-    } catch (e) {
-      pushNotification(e, 'error');
-    }
-  };
-
-  const handleReject = () => {
-    console.log('reject clicked');
-  };
+  const openModal = (name) => () => setOpenedModal(name);
 
   const increaseOrDecrease = difference > 0 ? 'increase' : 'decrease';
-  return (
-    <Container background="#FAFAFA" padding="24px 16px">
-      <Container display="flex" justifyContent="space-between">
-        <Container display="flex" flexDirection="column">
-          <Hint>High Level Insight</Hint>
-          <HorizontalSeparator height="5px" />
-          <Heading size="xl" color="#00664F">
-            £{getNumberWithCommas(total)}
-          </Heading>
-          <HorizontalSeparator height="5px" />
-          {`£${getNumberWithCommas(Math.abs(difference))} ${increaseOrDecrease} from last cycle`}
-        </Container>
-        <Container border="1px solid rgba(82, 90, 91, 0.25)" />
 
-        <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
-          <Heading size="xl" color="#00664F">
-            {suppliers}
-          </Heading>
-          Suppliers
-        </Container>
-        <Container border="1px solid rgba(82, 90, 91, 0.25)" />
-        <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
-          <Heading size="xl" color="#00664F">
-            {serviceUsers}
-          </Heading>
-          Service Users
-        </Container>
-        <Container border="1px solid rgba(82, 90, 91, 0.25)" />
-        <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
-          <Heading size="xl" color="#00664F">
-            {holdCount}
-          </Heading>
-          Holds worth £{getNumberWithCommas(holdValue)}
-        </Container>
-        <Container display="flex" flexDirection="column" alignSelf="center">
-          <Button onClick={handleApprove}>Approve</Button>
-          <HorizontalSeparator height="10px" />
-          <Button onClick={handleReject} outline>
-            Reject
-          </Button>
+  return (
+    <>
+      <ApproveDeclineModal
+        setOpenedModal={setOpenedModal}
+        openedModal={openedModal}
+        payRunId={payRunId}
+        update={update}
+      />
+      <Container background="#FAFAFA" padding="24px 16px">
+        <Container display="flex" justifyContent="space-between">
+          <Container display="flex" flexDirection="column">
+            <Hint>High Level Insight</Hint>
+            <HorizontalSeparator height="5px" />
+            <Heading size="xl" color="#00664F">
+              £{getNumberWithCommas(total)}
+            </Heading>
+            <HorizontalSeparator height="5px" />
+            {`£${getNumberWithCommas(Math.abs(difference))} ${increaseOrDecrease} from last cycle`}
+          </Container>
+          <Container border="1px solid rgba(82, 90, 91, 0.25)" />
+
+          <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
+            <Heading size="xl" color="#00664F">
+              {suppliers}
+            </Heading>
+            Suppliers
+          </Container>
+          <Container border="1px solid rgba(82, 90, 91, 0.25)" />
+          <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
+            <Heading size="xl" color="#00664F">
+              {serviceUsers}
+            </Heading>
+            Service Users
+          </Container>
+          <Container border="1px solid rgba(82, 90, 91, 0.25)" />
+          <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
+            <Heading size="xl" color="#00664F">
+              {holdCount}
+            </Heading>
+            Holds worth £{getNumberWithCommas(holdValue)}
+          </Container>
+          <Container display="flex" flexDirection="column" alignSelf="center">
+            <Button onClick={openModal('Approve')}>Approve</Button>
+            <HorizontalSeparator height="10px" />
+            <Button className="no-border link-button" color="red" secondary onClick={openModal('Decline')} outline>
+              Reject
+            </Button>
+          </Container>
         </Container>
       </Container>
-    </Container>
+    </>
   );
 };
