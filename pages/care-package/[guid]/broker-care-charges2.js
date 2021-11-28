7833bc56d5141a8920f92ffe6b8a6ed8e25217b3
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-11-28 23:55:43 +0000

    wip

--- pages/care-package/[guid]/broker-care-charges2.js
+++ pages/care-package/[guid]/broker-care-charges2.js
@@ -13,6 +13,9 @@ import {
   FormGroup,
   Input,
   RadioGroup,
+  Select,
+  WarningText,
+  Textarea,
 } from 'components';
 import { useRouter } from 'next/router';
 import { useDispatch } from 'react-redux';
@@ -31,6 +34,8 @@ import { getFormData } from 'service/getFormData';
 import { formValidationSchema } from 'service/formValidationSchema';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 import { reclaimType } from 'constants/variables';
+import { CareChargeSchedule } from 'components/Pages/CarePackages/BrokerCareCharge/CareChargeSchedule';
+import { FundingPerWeek } from 'components/Pages/CarePackages/BrokerCareCharge/FundingPerWeek';
 
 export const getServerSideProps = withSession(async ({ req }) => {
   const user = getLoggedInUser({ req });
@@ -77,27 +82,34 @@ const CareCharge = () => {
     defaultValues: {
       cost: null,
       claimCollector: 2,
+      claimReason: null,
+      startDate: null,
+      endDate: null,
+      isOngoing: false,
+      description: null,
     },
   });
 
   useEffect(() => {
     if (calculatedCost) setValue('cost', calculatedCost);
   }, [calculatedCost]);
-  // useEffect(() => {
-  //   if (fncData) {
-  //     setValue('startDate', fncData.startDate);
-  //     setValue('claimCollector', fncData.claimCollector);
-  //     if (fncData.id) setValue('id', fncData.id);
-  //     if (fncData.description) setValue('description', fncData.description);
-  //     if (fncData.assessmentFileName) setValue('assessmentFileName', fncData.assessmentFileName);
-  //     if (fncData.assessmentFileId) setValue('assessmentFileId', fncData.assessmentFileId);
-  //     if (!fncData.endDate && fncData.startDate) setValue('isOngoing', true);
-  //     if (fncData.endDate) {
-  //       setValue('endDate', fncData.endDate);
-  //       setValue('isOngoing', false);
-  //     }
-  //   }
-  // }, [fncLoading]);
+
+  useEffect(() => {
+    if (careChargeData && careChargeData.length) {
+      const data = careChargeData[0];
+      setValue('startDate', data.startDate);
+      setValue('claimCollector', data.claimCollector);
+      if (data.claimReason) setValue('claimReason', data.claimReason);
+      if (data.description) setValue('description', data.description);
+      if (data.assessmentFileName) setValue('assessmentFileName', data.assessmentFileName);
+      if (data.assessmentFileId) setValue('assessmentFileId', data.assessmentFileId);
+      if (!data.endDate && data.startDate) setValue('isOngoing', true);
+      if (data.endDate) {
+        setValue('endDate', data.endDate);
+        setValue('isOngoing', false);
+      }
+    }
+  }, [careChargeLoading]);
 
   const clickBack = () => router.push(getBrokerPackageRoute(carePackageId));
 
@@ -131,6 +143,16 @@ const CareCharge = () => {
     setIsRequestBeingSent(false);
   };
 
+  const collectingReasonOptions = [
+    { text: 'Service user unable to manage finances', value: '1' },
+    { text: 'Agreement with provider to pay gross', value: '2' },
+    { text: 'Service user or family declining payment', value: '3' },
+    { text: 'Finance managed by CFAT', value: '4' },
+    { text: 'Other', value: '5' },
+  ];
+  const collectedBy = watch('claimCollector');
+  const isOngoing = watch('isOngoing');
+  const cost = watch('cost');
   return (
     <>
       <DynamicBreadcrumbs />
@@ -140,7 +162,7 @@ const CareCharge = () => {
         {!careChargeLoading && (
           <form onSubmit={handleSubmit(updatePackage)}>
             <Container className="brokerage__container">
-              <Hint>Provisional care charge (pre-assessement)</Hint>
+              <WarningText>Provisional care charge (pre-assessement)</WarningText>
               <HorizontalSeparator height="30px" />
               <FormGroup label="Cost per week" hint="Auto calculated on age">
                 <Controller
@@ -159,24 +181,43 @@ const CareCharge = () => {
                 />
               </FormGroup>
             </Container>
-            <Controller
-              name="claimCollector"
-              control={control}
-              render={({ field }) => (
-                <RadioGroup
-                  handle={field.onChange}
-                  inline
-                  // error={errors.collectedBy}
-                  label="Collected by"
-                  items={[
-                    { id: 1, label: 'Hackney council (gross)' },
-                    { id: 2, label: 'Supplier (net)' },
-                  ]}
-                  {...field}
-                />
-              )}
-            />
+            <CareChargeSchedule control={control} errors={errors} isOngoing={isOngoing} />
 
+            <Container>
+              <Controller
+                name="claimCollector"
+                control={control}
+                render={({ field }) => (
+                  <RadioGroup
+                    handle={field.onChange}
+                    inline
+                    // error={errors.collectedBy}
+                    label="Collected by"
+                    items={[
+                      { id: 1, label: 'Hackney council (gross)' },
+                      { id: 2, label: 'Supplier (net)' },
+                    ]}
+                    {...field}
+                  />
+                )}
+              />
+              <HorizontalSeparator height="20px" />
+              <FormGroup label="Why is Hackney collecting these care charges?" disabled={collectedBy !== 1}>
+                <Controller
+                  name="claimReason"
+                  control={control}
+                  render={({ field }) => <Select options={collectingReasonOptions} {...field} />}
+                />
+                <HorizontalSeparator height="10px" />
+                <Controller
+                  name="description"
+                  control={control}
+                  render={({ field }) => <Textarea value={field.value} handler={field.onChange} />}
+                />
+              </FormGroup>
+            </Container>
+            <HorizontalSeparator height="48px" />
+            <FundingPerWeek total={cost} />
             <HorizontalSeparator height="48px" />
             <Container display="flex">
               <Button onClick={clickBack} secondary color="gray">
