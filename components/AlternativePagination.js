4431fdc34fd26fc53f9153965f5fe9e402e4b6bb
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-11-29 11:16:45 +0000

    Pagination skip to end (#350)
    * add pagination fix
    
    * fixed logic
    
    * fixe pagination

--- components/AlternativePagination.js
+++ components/AlternativePagination.js
@@ -39,14 +39,14 @@ const AlternativePagination = ({
       )}
 
       <p className="table-pagination-info">Showing {`${fromCalc}-${toCalc} of ${totalCount} items`}</p>
-
       <div className="table-pagination-actions">
+        {1 + siblingCount < currentPage && (
+          <Button onClick={() => onChangePagination(1)} className="govuk-button lbh-button table-pagination-button">
+            {1}
+          </Button>
+        )}
         {totalCount === 0 ? (
-          <Button
-            key="page-1"
-            onClick={() => onChangePagination(1)}
-            className="table-pagination-button table-pagination-item-active"
-          >
+          <Button key="page-1" onClick={() => onChangePagination(1)}>
             1
           </Button>
         ) : (
@@ -64,6 +64,14 @@ const AlternativePagination = ({
               );
             return null;
           })
+        )}
+        {Math.round(totalPages) > currentPage + siblingCount && (
+          <Button
+            onClick={() => onChangePagination(totalPages)}
+            className="govuk-button lbh-button table-pagination-button"
+          >
+            {Math.round(totalPages)}
+          </Button>
         )}
       </div>
     </div>
