07205ed8ed05813fb0617c42ec29a18900ff8fec
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-11-30 14:41:31 +0000

    High level insight update (#364)

--- components/Pages/Payruns/HighLevelInsight/index.js
+++ components/Pages/Payruns/HighLevelInsight/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Container, Heading, Hint, HorizontalSeparator } from 'components';
+import { Button, Container, Heading, Hint, HorizontalSeparator, Link } from 'components';
 import { getNumberWithCommas } from 'service';
 import ApproveDeclineModal from '../ApproveDeclineModal';
+import { InsightButtons } from './InsightButtons';
 
 export const HighLevelInsight = ({
   payRunId,
@@ -12,10 +13,13 @@ export const HighLevelInsight = ({
   holdCount = 0,
   holdValue = 0,
   update,
+  status,
+  isCedarFileDownloaded,
+  insightDataLoading,
 }) => {
   const [openedModal, setOpenedModal] = useState('');
 
-  const openModal = (name) => () => setOpenedModal(name);
+  // const openModal = (name) => () => setOpenedModal(name);
 
   const increaseOrDecrease = difference > 0 ? 'increase' : 'decrease';
 
@@ -38,35 +42,34 @@ export const HighLevelInsight = ({
             <HorizontalSeparator height="5px" />
             {`£${getNumberWithCommas(Math.abs(difference))} ${increaseOrDecrease} from last cycle`}
           </Container>
-          <Container border="1px solid rgba(82, 90, 91, 0.25)" />
-
+          <Container borderRight="1px solid rgba(82, 90, 91, 0.25)" />
           <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
             <Heading size="xl" color="#00664F">
               {suppliers}
             </Heading>
             Suppliers
           </Container>
-          <Container border="1px solid rgba(82, 90, 91, 0.25)" />
+          <Container borderRight="1px solid rgba(82, 90, 91, 0.25)" />
           <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
             <Heading size="xl" color="#00664F">
               {serviceUsers}
             </Heading>
             Service Users
           </Container>
-          <Container border="1px solid rgba(82, 90, 91, 0.25)" />
+          <Container borderRight="1px solid rgba(82, 90, 91, 0.25)" />
           <Container display="flex" flexDirection="column" textAlign="center" alignSelf="center">
             <Heading size="xl" color="#00664F">
               {holdCount}
             </Heading>
             Holds worth £{getNumberWithCommas(holdValue)}
           </Container>
-          <Container display="flex" flexDirection="column" alignSelf="center">
-            <Button onClick={openModal('Approve')}>Approve</Button>
-            <HorizontalSeparator height="10px" />
-            <Button className="no-border link-button" color="red" secondary onClick={openModal('Decline')} outline>
-              Reject
-            </Button>
-          </Container>
+          <InsightButtons
+            payRunId={payRunId}
+            status={status}
+            isCedarFileDownloaded={isCedarFileDownloaded}
+            update={update}
+            isLoading={insightDataLoading}
+          />
         </Container>
       </Container>
     </>
