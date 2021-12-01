580f7d43e59f47eb2526f19b3b571f855fe6b5f8
mugnivenko <m.ugnivenko@dunice.net>
2021-12-01 14:08:47 +0300

    add file link

--- components/HackneyDS/UploadGreenButton/index.js
+++ components/HackneyDS/UploadGreenButton/index.js
@@ -3,9 +3,9 @@ import UrlFromFile from '../../UrlFromFile';
 import FormGroup from '../FormGroup';
 import { FileUpload } from '../FileUpload/FileUpload';
 
-export const UploadGreenButton = ({ fileInfo, setFile, label, className, extensions }) => (
+export const UploadGreenButton = ({ fileInfo, isLoading, setFile, label, className, extensions }) => (
   <FormGroup className={`upload-green-button${className ? ` ${className}` : ''}`} label={label}>
-    <UrlFromFile file={fileInfo} removeFile={() => setFile(null)}/>
+    <UrlFromFile isLoading={isLoading} file={fileInfo} removeFile={() => setFile(null)}/>
     {!fileInfo && (
       <FormGroup className="file-upload-form">
         <FileUpload extensions={extensions} setFile={setFile} label={<div>Upload file</div>}/>
