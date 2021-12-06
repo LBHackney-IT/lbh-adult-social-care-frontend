05e58b963a226e31b4080cb8b808967f46aeb9a1
mugnivenko <m.ugnivenko@dunice.net>
2021-12-06 12:48:31 +0300

    Announcement refactoring

--- components/HackneyDS/Announcement/index.js
+++ components/HackneyDS/Announcement/index.js
@@ -5,7 +5,7 @@ const mainClass = 'lbh-announcement';
 export default function Announcement({ title, children, isWarning, className = '', isError }) {
   const isWarningClass = isWarning ? ` ${mainClass}--warning` : '';
   const isErrorClass = isError ? ` ${mainClass}--error` : '';
-  const themeClass = isErrorClass || isWarningClass || '';
+  const themeClass = isErrorClass || isWarningClass;
   return (
     <section className={`${mainClass} ${mainClass}--site${className && ` ${className}`}${themeClass}`}>
       <h3 className={`${mainClass}__title`}>{title}</h3>
