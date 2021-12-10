7baeb8b3534a63c2bbcd24bb696d425962436e1b
mugnivenko <m.ugnivenko@dunice.net>
2021-12-10 12:51:01 +0300

    added middleware

---
+++ pages/_middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+import { APP_SERVICE_ROUTES, APP_SERVICE_ROUTES_MAP } from '../routes/RouteConstants';
+
+export async function middleware (req) {
+  const { pathname } = req.nextUrl;
+  const { hackneyToken } = req.cookies;
+
+  // if route is (/login or /logout or /404) then just continue
+  if (APP_SERVICE_ROUTES_MAP.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  if (!hackneyToken) {
+    return NextResponse.redirect(APP_SERVICE_ROUTES.login);
+  }
+
+  return NextResponse.next();
+}
