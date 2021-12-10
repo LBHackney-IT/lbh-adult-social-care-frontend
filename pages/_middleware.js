51368aeb9f693d96a080a9c7434389ed2b2da66f
mugnivenko <m.ugnivenko@dunice.net>
2021-12-10 17:30:53 +0300

    fix middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,17 +1,13 @@
 import { NextResponse } from 'next/server';
 import { APP_SERVICE_ROUTES } from 'routes/RouteConstants';
 
+const APP_ROUTES = [APP_SERVICE_ROUTES.login, '/api/user', '/api/logout', '/api/login', '/favicon.ico'];
+
 export function middleware (req) {
   const { pathname } = req.nextUrl;
   const { hackneyToken } = req.cookies;
 
-  if ([APP_SERVICE_ROUTES.login, '/api/user', '/api/logout', '/api/login', '/favicon.ico'].includes(pathname)) {
-    return NextResponse.next();
-  }
-
-  if (!hackneyToken) {
-    return NextResponse.redirect(APP_SERVICE_ROUTES.login);
-  }
+  if (!hackneyToken && !APP_ROUTES.includes(pathname)) return NextResponse.redirect(APP_SERVICE_ROUTES.login);
 
   return NextResponse.next();
 }
--- pages/api/logout.js
+++ pages/api/logout.js
@@ -2,6 +2,5 @@ import withSession from 'lib/session';
 
 export default withSession((req, res) => {
   req.session.destroy();
-  console.log('logout destroy', req.session);
   res.end();
 });
