262fe04ba8eaadf8e425ca386afdde96add8dded
mugnivenko <m.ugnivenko@dunice.net>
2021-12-13 14:55:52 +0300

    test middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,10 +4,10 @@ import { APP_SERVICE_ROUTES } from 'routes/RouteConstants';
 const APP_ROUTES = [APP_SERVICE_ROUTES.login, '/api/user', '/api/logout', '/api/login', '/favicon.ico'];
 
 export function middleware (req) {
-  const { pathname } = req.nextUrl;
-  const { hackneyToken } = req.cookies;
-
-  if (!hackneyToken && !APP_ROUTES.includes(pathname)) return NextResponse.redirect(APP_SERVICE_ROUTES.login);
+  // const { pathname } = req.nextUrl;
+  // const { hackneyToken } = req.cookies;
+  //
+  // if (!hackneyToken && !APP_ROUTES.includes(pathname)) return NextResponse.redirect(APP_SERVICE_ROUTES.login);
 
   return NextResponse.next();
 }
