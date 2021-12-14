e6c37fc1bc143dd486a66853ceb363154e2f3618
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2021-12-14 14:21:52 +0000

    wip

--- components/UploadFile.js
+++ components/UploadFile.js
@@ -4,20 +4,26 @@ import { Controller } from 'react-hook-form';
 import { TEXT_FILE_EXTENSIONS } from 'constants/variables';
 
 const UploadFile = ({ control, isLoading, title, name = 'assessmentFile' }) => (
-    <div className="financial-assessment">
-      <Container>
-        {title && <HorizontalSeparator height={24} />}
-        <Controller
-          name={name}
-          control={control}
-          render={({ field }) => (
-            <FormGroup label={<Heading size="m">{title}</Heading>}>
-              <UploadGreenButton isLoading={isLoading} extensions={TEXT_FILE_EXTENSIONS} fileInfo={field.value} setFile={field.onChange} />
-            </FormGroup>
-          )}
-        />
-      </Container>
-    </div>
-  )
+  <div className="financial-assessment">
+    <Container>
+      {title && <HorizontalSeparator height={24} />}
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <FormGroup label={title}>
+            <HorizontalSeparator height="12px" />
+            <UploadGreenButton
+              isLoading={isLoading}
+              extensions={TEXT_FILE_EXTENSIONS}
+              fileInfo={field.value}
+              setFile={field.onChange}
+            />
+          </FormGroup>
+        )}
+      />
+    </Container>
+  </div>
+);
 
 export default memo(UploadFile);
