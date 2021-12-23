9f2cdbc48398fd324e9274163f966f76c7f739f0
mugnivenko <m.ugnivenko@dunice.net>
2021-12-23 18:20:42 +0300

    added height width props

--- components/ResetFilterButton.js
+++ components/ResetFilterButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Button } from './HackneyDS';
 
-const ResetFilterButton = ({ filters, onClearFilters, text = 'Clear', color = 'gray' }) => {
+const ResetFilterButton = ({ filters, width = 70, height = 40, onClearFilters, text = 'Clear', color = 'gray' }) => {
   if (!Object.values(filters).some((item) => item)) return null;
 
   return (
-    <Button outline secondary color={color} className="clear-filter-button" onClick={onClearFilters}>
+    <Button style={{ width, height }} outline secondary color={color} className="clear-filter-button" onClick={onClearFilters}>
       {text}
     </Button>
   );
