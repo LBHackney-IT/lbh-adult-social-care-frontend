8d1837f26eae321defc86c702f6de5022ea58ba8
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2022-01-04 13:29:23 +0000

    cleaning up code

--- pages/api/handleRoleBasedAccess.js
+++ pages/api/handleRoleBasedAccess.js
@@ -3,6 +3,5 @@ import { roleAccessMatrix } from './accessMatrix';
 export const handleRoleBasedAccess = (roles, url) => {
   const rolesWithAccess = roleAccessMatrix[url];
   const intersection = rolesWithAccess?.filter((value) => roles.includes(value));
-  console.log('intersection', intersection);
   return intersection?.length > 0;
 };
