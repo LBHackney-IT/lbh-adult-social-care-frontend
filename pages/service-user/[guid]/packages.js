2bb963b9502c18809e1fb326f371a8aa2d959609
kghugo <hugosum.dev@protonmail.com>
2022-01-06 11:22:22 +0000

    Fix keys for <PackageRequest> and packages page

--- pages/service-user/[guid]/packages.js
+++ pages/service-user/[guid]/packages.js
@@ -68,10 +68,10 @@ const Packages = ({ roles }) => {
                 p.packageStatus === 'Not Approved'
             )
             .map((p, index) => (
-              <>
+              <React.Fragment key={p.packageId}>
                 <PackageRequest packageRequest={p} roles={roles}/>
                 {index < packages.length - 1 && <HorizontalSeparator height="20px" />}
-              </>
+              </React.Fragment>
             ))}
         <HorizontalSeparator height="48px" />
         {packages &&
