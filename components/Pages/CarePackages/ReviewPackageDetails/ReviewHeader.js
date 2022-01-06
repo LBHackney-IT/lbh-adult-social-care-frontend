ebadb72548846ef5b39081e8912bd3335918a372
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2022-01-06 14:31:13 +0000

    hotfix - add nissing payment history link

--- components/Pages/CarePackages/ReviewPackageDetails/ReviewHeader.js
+++ components/Pages/CarePackages/ReviewPackageDetails/ReviewHeader.js
@@ -3,35 +3,50 @@ import { Container, VerticalSeparator } from '../../../HackneyDS';
 import TitleSubtitleHeader from '../TitleSubtitleHeader';
 import PackageDetailsButtons from './PackageDetailsButtons';
 
-export const ReviewHeader = ({ title, subTitle, goToHistory, showEditActions, buttons, additionalButtons }) => (
+export const ReviewHeader = ({
+  title,
+  subTitle,
+  goToHistory,
+  goToPaymentHistory,
+  showEditActions,
+  buttons,
+  additionalButtons,
+}) => (
   <>
     <Container className="brokerage__container-header brokerage__container">
       <TitleSubtitleHeader
         width=""
         title={title}
         subTitle={subTitle}
-        additionalComponent={additionalButtons &&
-          <Container className="package-actions" display="flex" alignItems="center" margin="30px 0 0 0">
-            <PackageDetailsButtons className="ml-0" buttons={additionalButtons} />
-            {showEditActions &&
-              <>
-                <VerticalSeparator width={16} />
-                |
-                <VerticalSeparator width={16} />
-                <PackageDetailsButtons className="ml-0" buttons={buttons} />
-              </>
-            }
-          </Container>
+        additionalComponent={
+          additionalButtons && (
+            <Container className="package-actions" display="flex" alignItems="center" margin="30px 0 0 0">
+              <PackageDetailsButtons className="ml-0" buttons={additionalButtons} />
+              {showEditActions && (
+                <>
+                  <VerticalSeparator width={16} />
+                  |
+                  <VerticalSeparator width={16} />
+                  <PackageDetailsButtons className="ml-0" buttons={buttons} />
+                </>
+              )}
+            </Container>
+          )
         }
-        link={!additionalButtons && (
-          <span
-            onClick={goToHistory}
-            className="lbh-color-blue font-size-19px package-history-link"
-          >
-          Package History
-        </span>
-        )
-        }>
+        link={
+          !additionalButtons && (
+            <Container display="flex">
+              <span onClick={goToHistory} className="lbh-color-blue font-size-19px package-history-link">
+                Package History
+              </span>
+              <VerticalSeparator width="15px" />
+              <span onClick={goToPaymentHistory} className="lbh-color-blue font-size-19px package-history-link">
+                Payment History
+              </span>
+            </Container>
+          )
+        }
+      >
         {showEditActions && !additionalButtons && <PackageDetailsButtons buttons={buttons} />}
       </TitleSubtitleHeader>
     </Container>
