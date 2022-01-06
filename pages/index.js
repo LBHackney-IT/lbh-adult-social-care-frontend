4cdef6cf8aaa3105c93c1edac040bcc6da4236d8
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2022-01-06 19:11:34 +0000

    hotfix - add menu items to 401 page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,37 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { Loading } from 'components';
 import { BROKERAGE_ROUTE, getPreviousPath } from 'routes/RouteConstants';
+import withSession from 'lib/session';
+import { getLoggedInUser } from 'service';
+import { handleRoleBasedAccess } from './api/handleRoleBasedAccess';
+import { accessRoutes } from './api/accessMatrix';
 
-export default function IndexPage () {
+export const getServerSideProps = withSession(({ req }) => {
+  const user = getLoggedInUser({ req });
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+  if (!handleRoleBasedAccess(user.roles ?? [], accessRoutes.APPROVALS)) {
+    return {
+      redirect: {
+        destination: '/401',
+        permanent: false,
+      },
+    };
+  }
+  return { props: { roles: user.roles } };
+});
+
+export default function IndexPage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace(getPreviousPath() || BROKERAGE_ROUTE);
+    router.replace(getPreviousPath() ?? BROKERAGE_ROUTE);
   }, []);
 
   return (
