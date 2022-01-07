4534506a8cbbd1fa17ab7f954a919e14c853e262
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2022-01-07 14:49:31 +0000

    hotfix - remove console warning

--- components/HackneyDS/Timeline/index.js
+++ components/HackneyDS/Timeline/index.js
@@ -1,26 +1,27 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import Link from '../lettering/Link';
 
 export default function Timeline({ timelines = [] }) {
   return (
     <ol className="lbh-timeline">
-      {timelines.map(({ gapBelow, actionNeeded, major, minor, header, innerElements }) => {
+      {timelines.map(({ gapBelow, actionNeeded, major, minor, header, innerElements }, index) => {
         const gapBelowClass = gapBelow ? ' lbh-timeline__event--gap-below' : '';
         const actionNeededClass = actionNeeded ? ' lbh-timeline__event--action-needed' : '';
         const majorClass = major ? ' lbh-timeline__event--major' : '';
         const minorClass = minor ? ' lbh-timeline__event--minor' : '';
         const calcClasses = `${actionNeededClass}${majorClass}${minorClass}${gapBelowClass}`;
         return (
-          <li className={`lbh-timeline__event${calcClasses}`}>
+          <li className={`lbh-timeline__event${calcClasses}`} key={index}>
             {header && (
               <h3 className={header.rewriteClass || 'lbh-heading-h3'}>
                 {header.link ? <Link text={header.text} noVisited /> : header.text}
               </h3>
             )}
-            {innerElements?.map((element) => {
+            {innerElements?.map((element, i) => {
               if (element.text) {
                 return (
-                  <p className={element.rewriteClass || 'lbh-body'}>
+                  <p key={i} className={element.rewriteClass || 'lbh-body'}>
                     {element.link ? <Link href={element.link} text={element.text} noVisited /> : element.text}
                   </p>
                 );
