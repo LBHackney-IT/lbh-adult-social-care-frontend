3a466a6d1ca0229baaad514a96e90f34babe4c23
kghugo <hugosum.dev@protonmail.com>
2022-01-07 14:46:46 +0000

    Remove guards in all pages

--- pages/brokerage/index.js
+++ pages/brokerage/index.js
@@ -15,14 +15,6 @@ import { accessRoutes } from '../api/accessMatrix';
 
 export const getServerSideProps = withSession(({ req }) => {
   const user = getLoggedInUser({ req });
-  if (!user) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    };
-  }
 
   if (!handleRoleBasedAccess(user.roles ?? [], accessRoutes.BROKERAGE)) {
     return {
--- pages/care-package/[guid]/broker-care-charges.js
+++ pages/care-package/[guid]/broker-care-charges.js
@@ -39,14 +39,6 @@ import { accessRoutes } from '../../api/accessMatrix';
 
 export const getServerSideProps = withSession(async ({ req }) => {
   const user = getLoggedInUser({ req });
-  if (!user) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    };
-  }
   if (!handleRoleBasedAccess(user.roles ?? [], accessRoutes.CARE_PACKAGE_BROKER_CARE_CHARGES)) {
     return {
       redirect: {
