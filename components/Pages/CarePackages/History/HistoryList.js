54e5e50f48f02ca1a958681ee7fcfe7da692f6ad
akeamwoodsND <89010589+akeamwoodsND@users.noreply.github.com>
2022-01-07 14:21:43 +0000

    History Timeline (#450)
    * add timeline component
    
    * add notes

--- components/Pages/CarePackages/History/HistoryList.js
+++ components/Pages/CarePackages/History/HistoryList.js
@@ -1,31 +1,36 @@
 import { useRouter } from 'next/router';
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { usePackageHistory } from 'api';
 import { formatDate } from 'service';
+import { Timeline } from 'components';
 
 const HistoryList = () => {
   const router = useRouter();
   const { guid: packageId } = router.query;
 
   const { data } = usePackageHistory(packageId);
+  const [history, setHistory] = useState([]);
 
-  return (
-    <div className="history__list">
-      {data.history?.map((item) => (
-        <div key={item.id}>
-          <p>{formatDate(item.dateCreated)}</p>
+  useEffect(() => {
+    if (data && data.history) {
+      const x = [];
+      data.history.forEach((element) => {
+        x.push({
+          header: {
+            text: `${formatDate(element.dateCreated)} - ${element.description}`,
+            rewriteClass: 'govuk-heading-m',
+          },
+          innerElements: [
+            { text: element?.requestMoreInformation, rewriteClass: 'lbh-body-s' },
+            { text: `by ${element.creatorName}`, rewriteClass: 'lbh-body-s' },
+          ],
+        });
+      });
+      setHistory(x);
+    }
+  }, [data]);
 
-          <div>
-            <span>{item.creatorName}</span>
-            <span className="history__list-dot">•</span>
-            <span>{item.description}</span>
-          </div>
-
-          {item.requestMoreInformation && <span>{item.requestMoreInformation}</span>}
-        </div>
-      ))}
-    </div>
-  );
+  return <div className="history__list">{history && <Timeline timelines={history} />}</div>;
 };
 
 export default memo(HistoryList);
