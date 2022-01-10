410eb5ac85838cc194eb3ece583543de5ea8f2c1
kghugo <hugosum.dev@protonmail.com>
2022-01-10 11:14:56 +0000

    avoid using children prop directly

--- components/Pages/CarePackages/BrokerCareCharge/PreviousCareChargeAnnouncement.js
+++ components/Pages/CarePackages/BrokerCareCharge/PreviousCareChargeAnnouncement.js
@@ -5,22 +5,18 @@ export const PreviousCareChargesAnnouncement = ({ visible, usePreviousCareCharge
   if (!visible) return null;
 
   return (
-    <Announcement
-      isWarning
-      title="Care charges have previously been added."
-      children={
-        <Container>
-          <p>Would you like to use previous care charge values?</p>
-          <HorizontalSeparator height={8} />
-          <Container display="flex">
-            <Button onClick={usePreviousCareCharge}>Yes, use previous care charges</Button>
-            <VerticalSeparator width={10} />
-            <Button onClick={useNewCareCharge} secondary color="gray">
-              No, add new care charges
-            </Button>
-          </Container>
+    <Announcement isWarning title="Care charges have previously been added.">
+      <Container>
+        <p>Would you like to use previous care charge values?</p>
+        <HorizontalSeparator height={8} />
+        <Container display="flex">
+          <Button onClick={usePreviousCareCharge}>Yes, use previous care charges</Button>
+          <VerticalSeparator width={10} />
+          <Button onClick={useNewCareCharge} secondary color="gray">
+            No, add new care charges
+          </Button>
         </Container>
-      }
-    />
+      </Container>
+    </Announcement>
   );
 };
