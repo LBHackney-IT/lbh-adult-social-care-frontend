4ddc0b9217e3374898ec67122cd87feeb724060d
kghugo <hugosum.dev@protonmail.com>
2022-01-22 11:02:00 +0000

    Build project as zip

--- esbuild.lambda.js
+++ esbuild.lambda.js
@@ -1,31 +1,10 @@
-const ZipPlugin = require('zip-webpack-plugin');
-const path = require('path');
+const esbuild = require('esbuild');
 
-const config = {
-  entry: {
-    handler: './_lambda.js',
-  },
-  output: {
-    filename: '[name]/index.js',
-    path: path.resolve(__dirname, 'dist/'),
-    libraryTarget: 'umd',
-  },
-  target: 'node',
-  mode: 'production',
-  optimization: { minimize: false },
-};
-// finally zip the output directory, ready to deploy
-const pluginConfig = {
-  plugins: Object.keys(config.entry).map(
-    (entryName) =>
-      new ZipPlugin({
-        path: path.resolve(__dirname, 'dist/'),
-        filename: entryName,
-        extension: 'zip',
-        include: [entryName],
-      })
-  ),
-};
-
-const webpackConfig = Object.assign(config, pluginConfig);
-module.exports = webpackConfig;
+esbuild
+  .build({
+    entryPoints: ['_lambda.js'],
+    bundle: true,
+    outfile: './dist/lambda.js',
+    platform: 'node',
+  })
+  .catch(() => process.exit(1));
